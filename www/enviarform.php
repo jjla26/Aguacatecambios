cb7835c0cb5295ea7521f55bbe164e3945820caa
Julio Lopez <julioj.lopeza@gmail.com>
2018-05-10 15:31:34 +0000

    enviando formulario y almacenando datos

--- www/enviarform.php
+++ www/enviarform.php
@@ -1,7 +1,7 @@
 <?php
 
 //session_start();
-//if (isset($_SESSION['user'])){
+//if (!empty($_SESSION['user'])){
 //    $fechaGuardada = $_SESSION['ultimoAcceso'];
 //
 //    $ahora = date("Y-n-j H:i:s");
@@ -30,83 +30,94 @@ date_default_timezone_set('America/Santiago');
 $current_date = date("Y-m-d H:i:s");
 
 
-/*if (isset($_SESSION['user']) && isset($_POST['cliente']) && isset($_POST['rut']) && isset($_POST['email'])&& isset($_POST['telefono']) && 
-isset($_POST['tranf']) && isset($_POST['tipodoc']) && isset($_POST['iddoc']) && isset($_POST['banco']) && isset($_POST['cuenta']) && isset($_POST['pesos']) && isset($_POST['bolivares'])){
+/*if (!empty($_SESSION['user']) && !empty($_POST['cliente']) && !empty($_POST['rut']) && !empty($_POST['email'])&& !empty($_POST['telefono']) && 
+!empty($_POST['tranf']) && !empty($_POST['tipodoc']) && !empty($_POST['iddoc']) && !empty($_POST['banco']) && !empty($_POST['cuenta']) && !empty($_POST['pesos']) && !empty($_POST['bolivares'])){
 */
 
-if (empty($_FILES['archivo']['name'])){
+/*if (empty($_FILES['archivo']['name'])){
 header("location: formulario.php?proceso=falta_indicar_fichero"); //o como se llame el formulario ..
 exit;
-}
+}*/
 
-if (isset($_POST['tasap']) && isset($_POST['cliente']) && isset($_POST['rut']) && isset($_POST['totalpesos']) &&  isset($_POST['email'])&& isset($_POST['telefono']) && 
-isset($_POST['transf2']) && isset($_POST['nombre']) && isset($_POST['tipodoc']) && isset($_POST['iddoc']) && isset($_POST['banco']) && 
-isset($_POST['cuenta']) && isset($_POST['pesos']) && isset($_POST['bolivares'])){
+if (!empty($_POST['tasap']) && !empty($_POST['cliente']) && !empty($_POST['rut']) && !empty($_POST['totalpesos']) &&  !empty($_POST['email'])&& !empty($_POST['telefono']) && 
+!empty($_POST['transf2']) && !empty($_POST['nombre']) && !empty($_POST['tipodoc']) && !empty($_POST['iddoc']) && !empty($_POST['banco']) && 
+!empty($_POST['cuenta']) && !empty($_POST['pesos']) && !empty($_POST['bolivares'])){
 
-  $tasap = $_POST['tasap'];
-  $totalpesos = $_POST['totalpesos10'];
-  $pesos = $_POST['pesos'];
-  
+echo  $tasap = $_POST['tasap'];
+echo "\n";
+echo  $totalpesos = $_POST['totalpesos10'];
+echo "\n";
+echo  $pesos = $_POST['pesos11'];
+echo "\n";  
   include 'conexion.php';
   
   $tasa = "SELECT Tasa from Tasa";
   $tasa = mysqli_query($conexion, $tasa);
   $tasa = mysqli_fetch_array($tasa);
-  $tasa = $tasa['Tasa'];
-  
+echo  $tasa = $tasa['Tasa'];
+echo "\n";  
   $tasaesp = "SELECT Tasa from Tasa1";
   $tasaesp = mysqli_query($conexion, $tasaesp);
   $tasaesp = mysqli_fetch_array($tasaesp);
-  $tasaesp = $tasaesp['Tasa'];
-  
-  if( $tasap == $tasa && $pesos >= 7000 && $totalpesos >= 7000){
+echo  $tasaesp = $tasaesp['Tasa'];
+echo "\n";  
+  if( $tasap == $tasa){
+    if ($pesos >= 10000 && $totalpesos >= 10000){
 
 
-  $cliente = $_POST['cliente'];
-  $rut = $_POST['rut'];
-  $email = $_POST['email'];
-  $telefono = $_POST['telefono'];
-  $transf = $_POST['transf2'];
-  
-  $nombre = $_POST['nombre'];
-  $nacionalidad = $_POST['tipodoc'];
-  $cedula = $_POST['iddoc'];
-  $banco = $_POST['banco'];
-  $cuenta = $_POST['cuenta'];
-  
-  $bolivares= $_POST['bolivares'];
-  $estatus = 'Internet';
-
+echo  $cliente = $_POST['cliente'];
+echo "\n";
+echo  $rut = $_POST['rut'];
+echo "\n";
+echo  $email = $_POST['email'];
+echo "\n";
+echo  $telefono = $_POST['telefono'];
+echo "\n";
+echo  $transf = $_POST['transf2'];
+echo "\n";  
+echo  $nombre = $_POST['nombre'];
+echo "\n";
+echo  $nacionalidad = $_POST['tipodoc'];
+echo "\n";
+echo  $cedula = $_POST['iddoc'];
+echo "\n";
+echo  $banco = $_POST['banco'];
+echo "\n";
+echo  $cuenta = $_POST['cuenta'];
+echo "\n";  
+echo  $bolivares= $_POST['bolivares11'];
+echo "\n";
+echo  $estatus = 'Internet';
+echo "\n";
 
   if($transf == 2){
 
-  $pesos1 = $_POST['pesos5'];
+  echo $pesos1 = $_POST['pesos8'];
   
-    if(isset($_POST['nombre2']) && isset($_POST['tipodoc2']) && isset($_POST['iddoc2']) && isset($_POST['banco2']) && 
-    isset($_POST['cuenta2']) && isset($_POST['pesos5']) && isset($_POST['bolivares5']) && $pesos>=7000){
+    if(!empty($_POST['nombre2']) && !empty($_POST['tipodoc2']) && !empty($_POST['iddoc2']) && !empty($_POST['banco2']) && 
+    !empty($_POST['cuenta2']) && !empty($_POST['pesos5']) && !empty($_POST['bolivares5']) && $pesos>=7000){
 
-      $nombre1 = $_POST['nombre2'];
-      $nacionalidad1 = $_POST['tipodoc2'];
-      $cedula1 = $_POST['iddoc2'];
-      $banco1 = $_POST['banco2'];
-      $cuenta1 = $_POST['cuenta2'];
-      
-      $bolivares1 = $_POST['bolivares5'];
+      echo $nombre1 = $_POST['nombre2'];
+      echo $nacionalidad1 = $_POST['tipodoc2'];
+      echo $cedula1 = $_POST['iddoc2'];
+      echo $banco1 = $_POST['banco2'];
+      echo $cuenta1 = $_POST['cuenta2'];
+      echo $bolivares1 = $_POST['bolivares8'];
      
       if(round((bcadd($pesos,$pesos1,15))) <= $totalpesos){
       
-        if( $totalpesos >= 14000 && $totalpesos < 100000 && $pesos >= 7000 && $pesos1 >= 7000){
+        if( $totalpesos >= 20000 && $totalpesos < 100000 && $pesos >= 10000){
 
-        $bolivares = bcmul($pesos,$tasa,15);
-        $bolivares1 = bcmul($pesos1,$tasa,15);
-        $bolivares = floor($bolivares);
-        $bolivares1 = floor($bolivares1);
-        $pesos = floor($pesos);
-        $pesos1 = floor($pesos1);
+        echo $bolivares = bcmul($pesos,$tasa,15);
+        echo $bolivares1 = bcmul($pesos1,$tasa,15);
+        echo $bolivares = floor($bolivares);
+        echo $bolivares1 = floor($bolivares1);
+        echo $pesos = floor($pesos);
+        echo $pesos1 = floor($pesos1);
         
-        $insertar =  "INSERT INTO transacciones1 (cliente, rut, Nombre_Apellido, Tipo_doc, Cedula, Cuenta_destino, Numero_cuenta, Total_pesos, Cantidad_pesos, Cantidad_bs, estatus) VALUES('$cliente', '$rut', '$nombre', '$nacionalidad', '$cedula', '$banco','$cuenta', '$totalPesos', '$pesos', '$bolivares', '$estatus') "; 
+        $insertar =  "INSERT INTO transacciones1 (tasa, cliente, rut, Nombre_Apellido, Tipo_doc, Cedula, Cuenta_destino, Numero_cuenta, Total_pesos, Cantidad_pesos, Cantidad_bs, Email, Telefono, estatus) VALUES('$tasa', '$cliente', '$rut', '$nombre', '$nacionalidad', '$cedula', '$banco','$cuenta', '$totalpesos', '$pesos', '$bolivares','$email','$telefono', '$estatus') "; 
         
-        $insertar1 = "INSERT INTO transacciones1 (cliente, rut, Nombre_Apellido, Tipo_doc, Cedula, Cuenta_destino, Numero_cuenta, Total_pesos, Cantidad_pesos, Cantidad_bs, estatus) VALUES('$cliente', '$rut', '$nombre1', '$nacionalidad1', '$cedula1', '$banco1', '$cuenta1','$totalPesos1', '$pesos1', '$bolivares1', '$estatus') ";
+        $insertar1 = "INSERT INTO transacciones1 (tasa, cliente, rut, Nombre_Apellido, Tipo_doc, Cedula, Cuenta_destino, Numero_cuenta, Total_pesos, Cantidad_pesos, Cantidad_bs, Email, Telefono, estatus) VALUES('$tasa', '$cliente', '$rut', '$nombre1', '$nacionalidad1', '$cedula1', '$banco1', '$cuenta1','$totalpesos1', '$pesos1', '$bolivares1','$email','$telefono', '$estatus') ";
         
         $insertar  = mysqli_query($conexion, $insertar);
         $insertar1 = mysqli_query($conexion, $insertar1);
@@ -114,20 +125,20 @@ isset($_POST['cuenta']) && isset($_POST['pesos']) && isset($_POST['bolivares']))
         if(!$insertar || !$insertar1){
 
 
-          echo '<script>alert("Error 3"); window.location="index.php"</script>';    
-          //header('Location: index.php');
+          echo '<script>alert("Error 3"); window.location="principal.php"</script>';    
+          //header('Location: principal.php');
               
           }else{
             
-          echo '<script>alert("Sus datos fueron enviados con exito y el número de su transaccion es el  "); window.location="index.php"</script>';    
-          //header('Location: index.php');  
+          echo '<script>alert("Sus datos fueron enviados con exito y el número de su transaccion es el  "); window.location="principal.php"</script>';    
+          //header('Location: principal.php');  
           //echo '<script>alert("Sus datos fueron enviados con exito. Agradecemos su confianza")"</script>';    
-          //header('Location: index.php');
+          //header('Location: principal.php');
           }
         
       }
 
-        if($totalpesos >= 14000 && $totalpesos >= 100000 && $pesos >= 7000 && $pesos1 >= 7000){
+        if($totalpesos >= 20000 && $totalpesos >= 100000 && $pesos >= 10000){
   
           $bolivares = bcmul($pesos,$tasa,15);
           $bolivares1 = bcmul($pesos1,$tasa,15);
@@ -136,31 +147,31 @@ isset($_POST['cuenta']) && isset($_POST['pesos']) && isset($_POST['bolivares']))
           $pesos = floor($pesos);
           $pesos1 = floor($pesos1);
           
-          $insertar =  "INSERT INTO transacciones1 (cliente, rut, Nombre_Apellido, Tipo_doc, Cedula, Cuenta_destino, Numero_cuenta, Total_pesos, Cantidad_pesos, Cantidad_bs, estatus) VALUES('$cliente', '$rut', '$nombre', '$nacionalidad', '$cedula', '$banco', '$cuenta', '$totalPesos', '$pesos', '$bolivares', '$estatus') "; 
+          $insertar =  "INSERT INTO transacciones1 (tasa, cliente, rut, Nombre_Apellido, Tipo_doc, Cedula, Cuenta_destino, Numero_cuenta, Total_pesos, Cantidad_pesos, Cantidad_bs, Email, Telefono, estatus) VALUES('$tasa', '$cliente', '$rut', '$nombre', '$nacionalidad', '$cedula', '$banco', '$cuenta', '$totalpesos', '$pesos', '$bolivares','$email','$telefono', '$estatus') "; 
           
-          $insertar1 = "INSERT INTO transacciones1 (cliente, rut, Nombre_Apellido, Tipo_doc, Cedula, Cuenta_destino, Numero_cuenta, Total_pesos, Cantidad_pesos, Cantidad_bs, estatus) VALUES('$cliente', '$rut', '$nombre1', '$nacionalidad1', '$cedula1', '$banco1', '$cuenta1', '$totalPesos1', '$pesos1', '$bolivares1', '$estatus') ";
+          $insertar1 = "INSERT INTO transacciones1 (tasa, cliente, rut, Nombre_Apellido, Tipo_doc, Cedula, Cuenta_destino, Numero_cuenta, Total_pesos, Cantidad_pesos, Cantidad_bs, Email, Telefono, estatus) VALUES('$tasa', '$cliente', '$rut', '$nombre1', '$nacionalidad1', '$cedula1', '$banco1', '$cuenta1', '$totalpesos1', '$pesos1', '$bolivares1','$email','$telefono', '$estatus') ";
           
           $insertar =  mysqli_query($conexion, $insertar);
           $insertar1 = mysqli_query($conexion, $insertar1);
           
           if(!$insertar || !$insertar1){
 
-            echo '<script>alert("Error 1"); window.location="index.php"</script>';    
-            //header('Location: index.php');
+            echo '<script>alert("Error 1"); window.location="principal.php"</script>';    
+            //header('Location: principal.php');
                 
             }else{
               
-            echo '<script>alert("Sus datos fueron enviados con exito y el número de su transaccion es el  "); window.location="index.php"</script>';    
-            //header('Location: index.php');  
+            echo '<script>alert("Sus datos fueron enviados con exito y el número de su transaccion es el  "); window.location="principal.php"</script>';    
+            //header('Location: principal.php');  
             //echo '<script>alert("Sus datos fueron enviados con exito. Agradecemos su confianza")"</script>';    
-            //header('Location: index.php');
+            //header('Location: principal.php');
             }
           }
     }else{
-      echo '<script>alert("Los cantidades a transferir no pueden ser mayores al deposito"); window.location="index.php"</script>';    
-            //header('Location: index.php');  
+      echo '<script>alert("Los cantidades a transferir no pueden ser mayores al deposito"); window.location="principal.php"</script>';    
+            //header('Location: principal.php');  
             //echo '<script>alert("Sus datos fueron enviados con exito. Agradecemos su confianza")"</script>';    
-            //header('Location: index.php');
+            //header('Location: principal.php');
       
     }
     
@@ -172,38 +183,58 @@ isset($_POST['cuenta']) && isset($_POST['pesos']) && isset($_POST['bolivares']))
     $bolivares = floor($bolivares);
     $pesos = floor($pesos);
     
-    $insertar =  "INSERT INTO transacciones1 (cliente, rut, Nombre_Apellido, Tipo_doc, Cedula, Cuenta_destino, Numero_cuenta, Total_pesos, Cantidad_pesos, Cantidad_bs, estatus) VALUES ('$cliente', '$rut', '$nombre', '$nacionalidad','$cedula', '$banco', '$cuenta', '$totalpesos', '$pesos', '$bolivares', '$estatus') "; 
+    $insertar =  "INSERT INTO transacciones1 (tasa, cliente, rut, Nombre_Apellido, Tipo_doc, Cedula, Cuenta_destino, Numero_cuenta, Total_pesos, Cantidad_pesos, Cantidad_bs, Email, Telefono, estatus) VALUES ('$tasa', '$cliente', '$rut', '$nombre', '$nacionalidad','$cedula', '$banco', '$cuenta', '$totalpesos', '$pesos', '$bolivares', '$email', '$telefono', '$estatus') "; 
     
     $insertar = mysqli_query($conexion, $insertar);
     
     if(!$insertar){
 
-      echo '<script>alert("Error 2"); window.location="index.php"</script>';    
-      //header('Location: index.php');
+      echo '<script>alert("Error 2"); window.location="principal.php"</script>';    
+      //header('Location: principal.php');
           
       }else{
         
-      echo '<script>alert("Sus datos fueron enviados con exito y el número de su transaccion es el  "); window.location="index.php"</script>';    
-      //header('Location: index.php');  
+      echo '<script>alert("Sus datos fueron enviados con exito y el número de su transaccion es el  "); window.location="principal.php"</script>';    
+      //header('Location: principal.php');  
       //echo '<script>alert("Sus datos fueron enviados con exito. Agradecemos su confianza")"</script>';    
-      //header('Location: index.php');  
+      //header('Location: principal.php');  
         
   
   
       }
   }else{
-    echo '<script>alert("Los cantidades a transferir no pueden ser mayores al deposito"); window.location="index.php"</script>';    
-            //header('Location: index.php');  
+    echo '<script>alert("Los cantidades a transferir no pueden ser mayores al deposito"); window.location="principal.php"</script>';    
+            //header('Location: principal.php');  
             //echo '<script>alert("Sus datos fueron enviados con exito. Agradecemos su confianza")"</script>';    
-            //header('Location: index.php');
+            //header('Location: principal.php');
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
   }
     }
     
   }else{
-  echo '<script>alert("Por favor envie los datos nuevamente, datos erroneos"); window.location="index.php"</script>';
+  echo '<script>alert("El mínimo a enviar por transferencia es de 10mil pesos!"); window.location="principal.php"</script>';
   
     
   }
+  }else{
+  echo '<script>alert("La TASA ha cambiado!.. Llenar el formulario nuevamente "); window.location="principal.php"</script>'; 
+}
+}else{
+  echo '<script>alert("Por favor llene todos los datos del formulario!!! "); window.location="principal.php"</script>';  
 }
   
 
@@ -222,15 +253,15 @@ $insertar1 = mysqli_query($insertar1, $conexion);
 
 if(!$insertar && !$insertar1){
 
-  echo '<script>alert("Intente Nuevamente"); window.location="index.php"</script>';    
-//header('Location: index.php');
+  echo '<script>alert("Intente Nuevamente"); window.location="principal.php"</script>';    
+//header('Location: principal.php');
     
 }else{
   
-  echo '<script>alert("Sus datos fueron enviados con exito y el número de su transaccion es el  "); window.location="index.php"</script>';    
-//header('Location: index.php');  
+  echo '<script>alert("Sus datos fueron enviados con exito y el número de su transaccion es el  "); window.location="principal.php"</script>';    
+//header('Location: principal.php');  
 //echo '<script>alert("Sus datos fueron enviados con exito. Agradecemos su confianza")"</script>';    
-//header('Location: index.php');
+//header('Location: principal.php');
 
     
 }}}else{
@@ -250,29 +281,28 @@ $insertar = mysqli_query($conexion, $insertar);
 
 if(!$insertar){
 
-  echo '<script>alert("Faltan campos por llenar2); window.location="index.php"</script>';    
-//header('Location: index.php');
+  echo '<script>alert("Faltan campos por llenar2); window.location="principal.php"</script>';    
+//header('Location: principal.php');
 //echo '<script>alert("Sus datos no fueron enviados, por favor intente de nuevo")"</script>';    
-//header('Location: index.php');
+//header('Location: principal.php');
     
 }else{
 
-  echo '<script>alert("Sus datos fueron enviados con existos y su numero de transaccion es el "); window.location="index.php"</script>';    
-//header('Location: index.php.php');
+  echo '<script>alert("Sus datos fueron enviados con existos y su numero de transaccion es el "); window.location="principal.php"</script>';    
+//header('Location: principal.php.php');
     
 //echo '<script>alert("Sus datos fueron enviados con exito. Agradecemos su confianza")"</script>';    
-//header('Location: index.php.php');
+//header('Location: principal.php.php');
 
     
 }}}else{
 
   
   
-  echo '<script>alert("Faltan campos por llenar. Intente de nuevo"); window.location="index.php"</script>';    
+  echo '<script>alert("Faltan campos por llenar. Intente de nuevo"); window.location="principal.php"</script>';    
     
-//header('Location: index.php');
+//header('Location: principal.php');
 
 }
 */
 ?>
-
--- www/pruebaarchivo.php
+++ www/pruebaarchivo.php
@@ -8,90 +8,34 @@ require("PHPMailer/class.smtp.php");
 $correo= $_POST['correo'];
 $nombre=$_POST['nombre'];
 
-//function send($correo,$nombre,$descripcion)
-//{
-//	$mail = new PHPMailer() ;
-//
-//	$body = '<table width="537" height="662" border="1">
-//  <tbody>
-//    <tr>
-//      <td width="253" height="94">Buenas tardes señor '.$nombre.'</td>
-//      <td width="557">'.$descripcion.'</td>
-//    </tr>
-//    <tr>
-//      <td colspan="2"><img src="http://www.comolohicieron.com.mx/wp-content/uploads/2015/03/Screen-Shot-2015-03-29-at-3.36.49-PM-816x497.png"></td>
-//    </tr>
-//  </tbody>
-//</table>';
-//				 				 
-//		$body .= "";
-//
-//		$mail->IsSMTP(); 
-//
-//		//Sustituye (ServidorDeCorreoSMTP)  por el host de tu servidor de correo SMTP
-//		$mail->Host = "mail.comolohicieron.com.mx";
-//		$mail->Port       = 587;  
-//		$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-//		
-//		//Sustituye  ( CuentaDeEnvio )  por la cuenta desde la que deseas enviar por ejem. prueba@domitienda.com  
-//		$mail->From     = "martin@comolohicieron.com.mx";
-//		$mail->FromName = "Martin Flores";
-//		$mail->Subject  = "Hola este es una prueba de mail";
-//		$mail->AltBody  = "Leer"; 
-//		$mail->MsgHTML($body);
-//
-//		// Sustituye  (CuentaDestino )  por la cuenta a la que deseas enviar por ejem. usuario@destino.com  
-//		$mail->AddAddress($correo,'');
-//		$mail->SMTPAuth = true;
-//
-//		// Sustituye (CuentaDeEnvio )  por la misma cuenta que usaste en la parte superior en este caso  prueba@midominio.com  y sustituye (ContraseñaDeEnvio)  por la contraseña que tenga dicha cuenta 
-//
-//		$mail->Username = "sucuenta@comolohicieron.com.mx";
-//		$mail->Password = "********"; 
-//
-//		if($mail->Send())
-//		{
-//			//return true;
-//			
-//			return $body; 
-//		}else
-//		{
-//			return false;
-//			die();
-//		}
-//	}
-
-function sendgmail($correo,$nombre)
+function send($correo,$nombre,$descripcion)
 {
 	$mail = new PHPMailer() ;
 
-	$body = " Hola, Estimado $nombre\n\r 
-	
-	Le informamos que su transferencia al banco $banco numero de cuenta $cuenta fue procesada bajo el número $comprobante de Banesco\n\r
-	
-	Adjunta se encuentra su boleta por el servicio prestado
-	
-	Gracias por la confianza
-	
-	Equipo Aguacatecambios
-	
-	";
-	
+	$body = '<table width="537" height="662" border="1">
+  <tbody>
+    <tr>
+      <td width="253" height="94">Buenas tardes señor '.$nombre.'</td>
+      <td width="557">'.$descripcion.'</td>
+    </tr>
+    <tr>
+      <td colspan="2"><img src="http://www.comolohicieron.com.mx/wp-content/uploads/2015/03/Screen-Shot-2015-03-29-at-3.36.49-PM-816x497.png"></td>
+    </tr>
+  </tbody>
+</table>';
 				 				 
 		$body .= "";
 
 		$mail->IsSMTP(); 
 
 		//Sustituye (ServidorDeCorreoSMTP)  por el host de tu servidor de correo SMTP
- 		$mail->Host = "smtp.gmail.com";		
-		$mail->Port       = 465;  
-		$mail->SMTPAuth = true;
-		$mail->SMTPSecure = "ssl"; 
+		$mail->Host = "mail.comolohicieron.com.mx";
+		$mail->Port       = 587;  
 		$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
 		
 		//Sustituye  ( CuentaDeEnvio )  por la cuenta desde la que deseas enviar por ejem. prueba@domitienda.com  
-		$mail->From     = "aguacatecambios@gmail.com";
-		$mail->FromName = "Aguacatecambios";
+		$mail->From     = "martin@comolohicieron.com.mx";
+		$mail->FromName = "Martin Flores";
 		$mail->Subject  = "Hola este es una prueba de mail";
 		$mail->AltBody  = "Leer"; 
 		$mail->MsgHTML($body);
@@ -101,10 +45,14 @@ function sendgmail($correo,$nombre)
 		$mail->SMTPAuth = true;
 
 		// Sustituye (CuentaDeEnvio )  por la misma cuenta que usaste en la parte superior en este caso  prueba@midominio.com  y sustituye (ContraseñaDeEnvio)  por la contraseña que tenga dicha cuenta 
-		$mail->Username = "aguacatecambios@gmail.com";
-		$mail->Password = "edu19471962"; 
+
+		$mail->Username = "support@aguacatecambios.com";
+		$mail->Password = "jose200520003"; 
+
 		if($mail->Send())
-		{			
+		{
+			//return true;
+			
 			return $body; 
 		}else
 		{
@@ -113,6 +61,58 @@ function sendgmail($correo,$nombre)
 		}
 	}
 
+//function sendgmail($correo,$nombre)
+//{
+//	$mail = new PHPMailer() ;
+//
+//	$body = " Hola, Estimado $nombre\n\r 
+//	
+//	Le informamos que su transferencia al banco $banco numero de cuenta $cuenta fue procesada bajo el número $comprobante de Banesco\n\r
+//	
+//	Adjunta se encuentra su boleta por el servicio prestado
+//	
+//	Gracias por la confianza
+//	
+//	Equipo Aguacatecambios
+//	
+//	";
+//	
+//				 				 
+//		$body .= "";
+//
+//		$mail->IsSMTP(); 
+//
+//		//Sustituye (ServidorDeCorreoSMTP)  por el host de tu servidor de correo SMTP
+// 		$mail->Host = "smtp.gmail.com";		
+//		$mail->Port       = 465;  
+//		$mail->SMTPAuth = true;
+//		$mail->SMTPSecure = "ssl"; 
+//		$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+//		
+//		//Sustituye  ( CuentaDeEnvio )  por la cuenta desde la que deseas enviar por ejem. prueba@domitienda.com  
+//		$mail->From     = "aguacatecambios@gmail.com";
+//		$mail->FromName = "Aguacatecambios";
+//		$mail->Subject  = "Hola este es una prueba de mail";
+//		$mail->AltBody  = "Leer"; 
+//		$mail->MsgHTML($body);
+//
+//		// Sustituye  (CuentaDestino )  por la cuenta a la que deseas enviar por ejem. usuario@destino.com  
+//		$mail->AddAddress('julioj.lopeza@gmail.com','Julio');
+//		$mail->SMTPAuth = true;
+//
+//		// Sustituye (CuentaDeEnvio )  por la misma cuenta que usaste en la parte superior en este caso  prueba@midominio.com  y sustituye (ContraseñaDeEnvio)  por la contraseña que tenga dicha cuenta 
+//		$mail->Username = "aguacatecambios@gmail.com";
+//		$mail->Password = "edu19471962"; 
+//		if($mail->Send())
+//		{			
+//			return $body; 
+//		}else
+//		{
+//			return false;
+//			die();
+//		}
+//	}
+//
+//
 
-
-$html = sendgmail($correo,$nombre,$descripcion);
+// $html = sendgmail($correo,$nombre,$descripcion);
